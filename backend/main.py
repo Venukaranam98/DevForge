64e679eea241e05d18f391cb7b91f7066e8c83c8
venukaranam98 <vk3344556677@gmail.com>
2026-05-29 13:06:34 +0530

    Fixed history path for deployment

--- backend/main.py
+++ backend/main.py
@@ -1,113 +1,72 @@
 from fastapi import FastAPI
-
 from fastapi.middleware.cors import CORSMiddleware
-
 from fastapi.responses import FileResponse
-
 from pydantic import BaseModel
-
 from generator import create_project
-
 import json
-
+import os
 
 app = FastAPI()
 
-
-# CORS
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 app.add_middleware(
-
     CORSMiddleware,
-
     allow_origins=["*"],
-
     allow_credentials=True,
-
     allow_methods=["*"],
-
     allow_headers=["*"]
-
 )
 
-
-# REQUEST SCHEMA
-
 class ProjectSchema(BaseModel):
-
     project_name: str
-
     frontend: str
-
     backend: str
-
     database: str
-
     project_type: str
 
-
-# HOME ROUTE
-
 @app.get("/")
-
 def home():
-
     return {
-
         "success": True,
-
         "message": "DevForge Backend Running",
-
         "data": None
-
     }
 
-
-# GENERATE PROJECT ROUTE
-
 @app.post("/generate")
-
 def generate_project(project: ProjectSchema):
 
     zip_path = create_project(
-
         project.project_name,
-
         project.frontend,
-
         project.backend,
-
         project.database,
-
         project.project_type
-
     )
 
     return FileResponse(
-
         path=zip_path,
-
         filename=f"{project.project_name}.zip",
-
         media_type="application/zip"
-
     )
 
-
 @app.get("/history")
-
 def get_history():
 
-    with open("history.json", "r") as file:
+    history_file = os.path.join(BASE_DIR, "history.json")
 
+    if not os.path.exists(history_file):
+        return {
+            "success": True,
+            "message": "No history found",
+            "data": []
+        }
+
+    with open(history_file, "r") as file:
         history = json.load(file)
 
     return {
-
         "success": True,
-
         "message": "Project history fetched",
-
         "data": history
-
     }
